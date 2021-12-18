3bc1cf9f4e0ebe1d9d64329a46d199aa48344b46
Diego Imbert <diegoimbert@pm.me>
2021-12-18 22:03:31 +0100

    Removed svelte and auth stuff

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,21 +3,9 @@ require("dotenv").config();
 const { postgraphile } = require("postgraphile");
 const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-
-const port = process.env.PORT ?? 4000;
-
 const { pool } = require("./pgDb");
-const { setupTwitchAPIAuth } = require("./auth/twitch-api-auth");
 
 const app = express();
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-setupTwitchAPIAuth(app);
-
 app.use(
   postgraphile(pool, "tc", {
     appendPlugins: [PgSimplifyInflectorPlugin],
@@ -29,10 +17,11 @@ app.use(
     ignoreRBAC: false,
     disableDefaultMutations: true,
     pgSettings: async (req) => ({
-      "user.id": req.session?.passport?.user?.id,
+      user_id: req.locals?.user?.id,
     }),
   })
 );
+const port = process.env.PORT ?? 4000;
 const server = app.listen(port, () => {
   const href = `http://localhost:${port}/graphiql`;
   console.log(`PostGraphiQL available at ${href} 🚀`);
